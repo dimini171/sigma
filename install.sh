d276ffab73feeffa10c793e4d2cf24a6db74c8bb
luna <luna@hackclub.app>
2025-03-09 17:31:04 +0800

    updated install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,10 @@ MIN_SIZE=100000
 # user confirmation
 echo "This script installs ${REPO_OWNER}/${REPO_NAME} to ${INSTALL_PATH}"
 echo "Note: sudo access is required to install to ${INSTALL_PATH}"
-read -r -n 1 -p "Proceed? (y/N) " CONTINUE
+read -r -n 1 -p "Proceed? (y/N) " CONTINUE < /dev/tty
+
 echo ""
+
 [[ "$CONTINUE" =~ [yY] ]] || { echo "Exiting..."; exit 0; }
 
 # os validation
